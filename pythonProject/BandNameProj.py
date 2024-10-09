77302662ef3cdde6af778866dbbcbf1a66f108ec
unknown <ashleyjonathan353.com>
2024-10-09 05:59:23 +0100

    new changes

--- pythonProject/BandNameProj.py
+++ pythonProject/BandNameProj.py
@@ -1,1 +1,9 @@
-print("Hello")
+#prints out the header
+print("Welcome to the Band Name Generator\n")
+#Takes in input from user and stores in variables city and pet respectively
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What is your pet's name?\n")
+#concatinates the results and stores in a variable bandname that can be called later
+bandname = city + " " + pet
+#print the result as suggestion for bandname
+print("Your band name could be " + bandname)
